e7a921451b2f8c8b9dd80c3eb671085ccf3671f5
Konrad Siek <konrad.siek@gmail.com>
2021-04-14 01:02:25 +0200

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use std::path::Path;
+
+use djanco::*;
+use djanco::data::*;
+use djanco::log::*;
+use djanco::csv::*;
+use djanco::objects::*;
+use djanco_ext::*;
+
+#[djanco(May, 2021, subsets(All))]
+pub fn my_query(database: &Database, _log: &Log, output: &Path) -> Result<(), std::io::Error>  {
+    database.projects()        
+        .filter_by(AtLeast(Count(FromEachIf(project::Paths, Equal(path::Language, Language::Java))), 1))
+        .filter_by(AtLeast(project::Stars, 50))
+        .into_csv_in_dir(output, "s1_a_study_of_potential_code_borrowing.csv")
+}
